bdf6b448872e1a83cd79dc7212a1332a6384f22d
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-05 17:45:05 -0300

    Remove todos as variáveis declaradas com o let para restringir o máximo de mutabilidade.

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -4,9 +4,11 @@ const path = require('path');
 function lerDiretorio(caminho) {
   return new Promise((resolve, reject) => {
     try {
-      let arquivos = fs.readdirSync(caminho);
-      arquivos = arquivos.map((arquivo) => path.join(caminho, arquivo));
-      resolve(arquivos);
+      const arquivos = fs.readdirSync(caminho);
+      const arquivosCompletos = arquivos.map((arquivo) => {
+        return path.join(caminho, arquivo);
+      });
+      resolve(arquivosCompletos);
     } catch (e) {
       reject(e);
     }
@@ -56,13 +58,9 @@ function removerElementosSeApenasNumero(array) {
 function removerSimbolos(simbolos) {
   return function (array) {
     return array.map((el) => {
-      let textoSemSimbolos = el;
-
-      simbolos.forEach((simbolo) => {
-        textoSemSimbolos = textoSemSimbolos.split(simbolo).join('');
-      });
-
-      return textoSemSimbolos;
+      return simbolos.reduce((accumulator, currentValue) => {
+        return accumulator.split(currentValue).join('');
+      }, el);
     });
   };
 }
