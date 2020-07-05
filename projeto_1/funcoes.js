fc9b01d509b23a5ac3b993e238200d34cad73833
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-05 15:00:05 -0300

    Cria métodos para filtrar arquivos com determinada extensão, método para ler conteúdo de um arquivo específico e ler conteúdos de todos os arquivos.

--- projeto_1/funcoes.js
+++ projeto_1/funcoes.js
@@ -13,6 +13,29 @@ function lerDiretorio(caminho) {
   });
 }
 
+function elementosTerminadosCom(array, padrao) {
+  /* const regex = new RegExp(`\\b${padrao}$\\b`);
+  return array.filter((el) => regex.test(el)); */
+  return array.filter((el) => el.endsWith(padrao));
+}
+
+function lerArquivo(caminho) {
+  return new Promise((resolve, reject) => {
+    try {
+      const conteudo = fs.readFileSync(caminho, { encoding: 'utf-8' });
+      resolve(conteudo.toString());
+    } catch (e) {
+      reject(e);
+    }
+  });
+}
+
+function lerArquivos(caminhos) {
+  return Promise.all(caminhos.map(lerArquivo));
+}
+
 module.exports = {
   lerDiretorio,
+  elementosTerminadosCom,
+  lerArquivos,
 };
--- projeto_1/index.js
+++ projeto_1/index.js
@@ -3,4 +3,7 @@ const fn = require('./funcoes');
 
 const caminho = path.join(__dirname, '..', 'legendas');
 
-fn.lerDiretorio(caminho).then((arquivos) => console.log(arquivos));
+fn.lerDiretorio(caminho)
+  .then((arquivos) => fn.elementosTerminadosCom(arquivos, '.srt'))
+  .then(fn.lerArquivos)
+  .then((arquivos) => console.log(arquivos));
