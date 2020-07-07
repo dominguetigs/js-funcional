b0609312a66ecf53f3ca8060a964a5bdf1d82053
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-07 18:59:58 -0300

    Exemplo de lazy evaluation.

---
+++ funcional/lazy_evaluation.js
@@ -0,0 +1,31 @@
+function eager(a, b) {
+  const fim = Date.now() + 2500;
+  while (Date.now() < fim) {}
+
+  const valor = Math.pow(a, 3);
+  return valor + b;
+}
+
+function lazy(a) {
+  const fim = Date.now() + 2500;
+  while (Date.now() < fim) {}
+
+  const valor = Math.pow(a, 3);
+
+  return function (b) {
+    return valor + b;
+  };
+}
+
+console.time('#1');
+console.log(eager(3, 100));
+console.log(eager(3, 200));
+console.log(eager(3, 300));
+console.timeEnd('#1');
+
+console.time('#2');
+const lazy3 = lazy(3);
+console.log(lazy3(100));
+console.log(lazy3(200));
+console.log(lazy3(300));
+console.timeEnd('#2');
