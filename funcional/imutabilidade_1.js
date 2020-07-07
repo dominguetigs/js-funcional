0c48310728e284c8fbe22c7a53ce3abdddcb667d
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-07 16:27:22 -0300

    Ajustes no primeiro exemplo de imutabilidade.

--- funcional/imutabilidade_1.js
+++ funcional/imutabilidade_1.js
@@ -1,8 +1,12 @@
 function ordenar(array) {
-  return array.sort();
+  return [...array].sort();
 }
 
 const numeros = [3, 1, 7, 9, 4, 6];
-ordenar(numeros);
+const numerosOrdenados = ordenar(numeros);
 
-console.log(numeros);
+console.log(numeros, numerosOrdenados);
+
+const partesDosNumeros = numeros.slice(1);
+
+console.log(partesDosNumeros, numeros);
