c9089df7f6df7d9b6166c31d7e54bec2061847df
Gustavo Domingueti <gustavo.s.domingueti@gmail.com>
2020-07-18 16:21:44 -0300

    :card_index: Finalizado segundo exemplo de Operadores

--- rxjs/operadores_2.js
+++ rxjs/operadores_2.js
@@ -1,5 +1,20 @@
+const { XMLHttpRequest } = require('xmlhttprequest');
+
+const { ajax } = require('rxjs/ajax');
+
 const { interval } = require('rxjs');
 const { map, concatAll } = require('rxjs/operators');
+
+ajax({
+  createXHR: () => new XMLHttpRequest(),
+  url: 'https://api.github.com/users/cod3rcursos/repos',
+})
+  .pipe(
+    map((response) => JSON.parse(response.xhr.responseText)),
+    concatAll(),
+    map((repositorio) => repositorio.full_name)
+  )
+  .subscribe(console.log);
 
 /* interval(1000)
   .pipe(
